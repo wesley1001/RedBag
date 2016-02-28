ad428c1252b94f833dc6ad0c8a829179f510ebc3
wenyan694434007 <Speed.Yan@starlight-sms.com>
2016-02-28 22:12:28 +0800

    add file

---
+++ MicroWebsite/Models/AdvModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DataAccessLayer;
+
+namespace MicroWebsite.Models
+{
+    public class AdvListModel
+    {
+        public IList<AdvInfo> Advs { get; set; }
+    }
+}
