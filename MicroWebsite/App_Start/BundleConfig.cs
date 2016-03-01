302f5e7f0ca9c4eb5ffb817d0634125cd0d85a5a
WenYan694434007 <Speed.Yan@starlight-sms.com>
2016-03-01 18:27:33 +0800

    update

--- MicroWebsite/App_Start/BundleConfig.cs
+++ MicroWebsite/App_Start/BundleConfig.cs
@@ -13,7 +13,11 @@ namespace MicroWebsite
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
-
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js", "~/Scripts/bootstrap-dropdown-hover.min.js",
+                      "~/Scripts/bootbox.min.js", "~/Scripts/respond.js"));
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css"));
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
